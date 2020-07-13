ae7b861d8c3358f893ea7b8b803280783e97ead3
Riley Flynn <riley@rileyflynn.me>
2020-07-13 20:38:29 -0230

    Add chart

--- chart.py
+++ chart.py
@@ -1,8 +1,10 @@
 import os
 from typing import Dict
 
+import pygal
 import requests
 from dotenv import load_dotenv
+from pygal.style import Style
 
 load_dotenv()
 
@@ -12,7 +14,7 @@ resp = requests.post(
         "query": """
             {
                 viewer {
-                    repositories(first: 100) {
+                    repositories(first: 100, ownerAffiliations: [OWNER], isFork: false) {
                         nodes {
                             name
                             languages(first: 20) {
@@ -44,4 +46,22 @@ for repository in resp.json()["data"]["viewer"]["repositories"]["nodes"]:
 
         if lang_name not in language_colours:
             language_colours[lang_name] = language["node"]["color"]
+
+for language, colour in language_colours.items():
+    if colour is None:
+        language_colours[language] = "#ababab"
+
+language_counts = {
+    key: language_counts[key]
+    for key in sorted(language_counts, key=lambda k: language_counts[k], reverse=True)[:10]
+}
 print(language_counts, language_colours)
+
+style = Style(colors=list(language_colours[lang] for lang in language_counts))
+
+chart = pygal.Pie(style=style)
+
+for language, byte_count in language_counts.items():
+    chart.add(language, byte_count)
+
+chart.render_to_file("languages.svg")
