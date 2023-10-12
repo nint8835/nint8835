0fb029c12bdc9a70ab1c3b4a6b79564d101b583e
Riley Flynn <riley@rileyflynn.me>
2023-10-11 23:47:55 -0230

    Fix having greater than 100 repos; General modernization

--- chart.py
+++ chart.py
@@ -1,5 +1,5 @@
 import os
-from typing import Dict
+from typing import Any, Dict, Iterator
 
 import pygal
 import requests
@@ -13,49 +13,74 @@ load_dotenv()
 # deemed unlikely to ever appear in this graph.
 FILLER_COLOURS = ["#E8274B", "#64C800", "#9DC3FF", "#a957b0", "#cd6400"]
 
-resp = requests.post(
-    "https://api.github.com/graphql",
-    json={
-        "query": """
-            {
-                viewer {
-                    repositories(
-                    first: 100
-                    ownerAffiliations: [OWNER]
-                    isFork: false
-                    orderBy: {field: UPDATED_AT, direction: DESC}
-                    ) {
-                        nodes {
-                            name
-                            languages(first: 20) {
-                                edges {
-                                    size
-                                    node {
-                                        name
-                                        color
-                                    }
-                                }
-                            }
-                            repositoryTopics(first: 10) {
-                                nodes {
-                                    topic {
-                                        name
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
+LANGUAGES_QUERY = """
+query ($first: Int, $after: String) {
+  viewer {
+    repositories(
+      first: $first
+      after: $after
+      ownerAffiliations: [OWNER]
+      isFork: false
+      orderBy: {field: UPDATED_AT, direction: DESC}
+    ) {
+      pageInfo {
+        hasNextPage
+        endCursor
+      }
+      nodes {
+        name
+        languages(first: 20) {
+          edges {
+            size
+            node {
+              name
+              color
             }
-        """
-    },
-    headers={"Authorization": f"Bearer {os.environ['GITHUB_PAT']}"},
-)
+          }
+        }
+        repositoryTopics(first: 10) {
+          nodes {
+            topic {
+              name
+            }
+          }
+        }
+      }
+    }
+  }
+}
+"""
+
+
+def fetch_repos() -> Iterator[Dict[str, Any]]:
+    variables = {"first": 100}
+    has_next_page = True
+
+    while has_next_page:
+        resp = requests.post(
+            "https://api.github.com/graphql",
+            json={"query": LANGUAGES_QUERY, "variables": variables},
+            headers={"Authorization": f"Bearer {os.environ['GITHUB_PAT']}"},
+        )
+        resp.raise_for_status()
+
+        data = resp.json()["data"]
+        repositories_resp = data["viewer"]["repositories"]
+
+        has_next_page = repositories_resp["pageInfo"]["hasNextPage"]
+        variables["after"] = repositories_resp["pageInfo"]["endCursor"]
+
+        yield from repositories_resp["nodes"]
+
+
 language_counts: Dict[str, int] = {}
 language_colours: Dict[str, str] = {"Other": FILLER_COLOURS.pop()}
 
-for repository in resp.json()["data"]["viewer"]["repositories"]["nodes"]:
-    if any(topic["topic"]["name"] == "language-stats-ignored" for topic in repository["repositoryTopics"]["nodes"]):
+for repository in fetch_repos():
+    if any(
+        topic["topic"]["name"] == "language-stats-ignored"
+        for topic in repository["repositoryTopics"]["nodes"]
+    ):
         continue
     for language in repository["languages"]["edges"]:
         lang_name = language["node"]["name"]
@@ -70,15 +95,14 @@ for language, colour in language_colours.items():
     if colour is None:
         language_colours[language] = FILLER_COLOURS.pop()
 
-sorted_languages = sorted(language_counts, key=lambda k: language_counts[k], reverse=True)
+sorted_languages = sorted(
+    language_counts, key=lambda k: language_counts[k], reverse=True
+)
 
-top_language_counts = {
-    key: language_counts[key]
-    for key in sorted_languages[
-        :10
-    ]
-}
-top_language_counts["Other"] = sum(language_counts[key] for key in sorted_languages[10:])
+top_language_counts = {key: language_counts[key] for key in sorted_languages[:10]}
+top_language_counts["Other"] = sum(
+    language_counts[key] for key in sorted_languages[10:]
+)
 
 style = Style(colors=list(language_colours[lang] for lang in top_language_counts))
 
--- noxfile.py
+++ noxfile.py
@@ -4,21 +4,21 @@ import nox.sessions
 PACKAGE_FILES = ["chart.py", "noxfile.py"]
 
 
-@nox.session(python="3.8")
+@nox.session(python="3.11")
 def lint(session: nox.sessions.Session) -> None:
-    session.install("flake8==3.7.9", "black==19.10b0", "mypy==0.770")
+    session.install("-r", "requirements.txt")
     session.run("flake8", *PACKAGE_FILES)
     session.run("black", "--check", *PACKAGE_FILES)
 
 
-@nox.session(python="3.8")
+@nox.session(python="3.11")
 def typecheck(session: nox.sessions.Session) -> None:
     session.install("-r", "requirements.txt")
     session.run("mypy", *PACKAGE_FILES)
 
 
-@nox.session(python="3.8")
+@nox.session(python="3.11")
 def format(session: nox.sessions.Session) -> None:
-    session.install("black==19.10b0", "isort==4.3.21")
-    session.run("isort", "--recursive", *PACKAGE_FILES)
+    session.install("-r", "requirements.txt")
+    session.run("isort", *PACKAGE_FILES)
     session.run("black", *PACKAGE_FILES)
